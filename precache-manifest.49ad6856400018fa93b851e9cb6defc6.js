aa20bb2db2d8b6e135662cf8289842b735e976ce
Eugene Van Zyl <eugenevanzyl@Eugenes-MacBook.local>
2018-11-25 15:23:44 +0200

    Updates

---
+++ precache-manifest.49ad6856400018fa93b851e9cb6defc6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "63d4957d197b11ab50b155ca39e2711a",
+    "url": "/trilloproject/static/media/eugeva.63d4957d.jpeg"
+  },
+  {
+    "revision": "73f386a2d5ea5c843736",
+    "url": "/trilloproject/static/css/main.08c35e10.chunk.css"
+  },
+  {
+    "revision": "cbc9ff6e77588073a765",
+    "url": "/trilloproject/static/js/1.cbc9ff6e.chunk.js"
+  },
+  {
+    "revision": "6d464120906abac9caac",
+    "url": "/trilloproject/static/js/runtime~main.6d464120.js"
+  },
+  {
+    "revision": "9c587a50aa216bea23193d446e3d7541",
+    "url": "/trilloproject/static/media/sprite.9c587a50.svg"
+  },
+  {
+    "revision": "73f386a2d5ea5c843736",
+    "url": "/trilloproject/static/js/main.73f386a2.chunk.js"
+  },
+  {
+    "revision": "5a550a3da0d63bfa7ec73ac1f1acdbba",
+    "url": "/trilloproject/static/media/hotel-1.5a550a3d.jpg"
+  },
+  {
+    "revision": "5468a3b162abcbb501141ce0e7f4aa6e",
+    "url": "/trilloproject/static/media/hotel-2.5468a3b1.jpg"
+  },
+  {
+    "revision": "7af5efbc95e0464867f4327aee208ad5",
+    "url": "/trilloproject/static/media/hotel-3.7af5efbc.jpg"
+  },
+  {
+    "revision": "0670a98b8aa1a7eb0a0c37f3cdfba06b",
+    "url": "/trilloproject/static/media/chevron-thin-right.0670a98b.svg"
+  },
+  {
+    "revision": "b1b5266aefb886ae95e677c1174930c9",
+    "url": "/trilloproject/index.html"
+  }
+];
